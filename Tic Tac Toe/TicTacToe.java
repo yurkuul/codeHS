b1ee47cb8c31074beb1b0545235cd4e104182d6a
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2021-12-11 19:52:03 -0600

    Update TicTacToe.java

--- Tic Tac Toe/TicTacToe.java
+++ Tic Tac Toe/TicTacToe.java
@@ -1,24 +1,38 @@
-public class TicTacToe {
-  private int turn;
-  private char[][] board = {{' ', '0', '1', '2'},
+public class TicTacToe
+{
+   private int turn;
+   private char[][] board = {{' ', '0', '1', '2'},
                             {'0', '-', '-', '-'},
                             {'1', '-', '-', '-'},
                             {'2', '-', '-', '-'}};
    
-  public TicTacToe () {
-    turn = 0;
-  }
+   public TicTacToe () {
+       turn = 0;
+   }
    
-  public int getTurn() {
-    return turn;
-  }
+   public int getTurn() {
+       return turn;
+   }
+
+   public void printBoard() {
+       for (int r = 0; r < board.length; r++) {
+           for (int c = 0; c < board[r].length; c++) {
+              System.out.print (board[r][c] + " ");
+           }
+           System.out.println();
+       }
+   }
    
-  public void printBoard() {
-    for (int r = 0; r < board.length; r++) {
-      for (int c = 0; c < board[r].length; c++) {
-        System.out.print (board[r][c] + " ");
-      }
-      System.out.println();
-    }
-  }
+   public boolean pickLocation(int row, int col) {
+       return board[row+1][col+1] == '-';
+   }
+   
+   public void takeTurn(int row, int col) {
+       if (turn % 2 == 0) {
+          board[row+1][col+1] = 'X';
+       } else {
+          board[row+1][col+1] = 'O';
+       }
+       turn++;
+   }
 }
