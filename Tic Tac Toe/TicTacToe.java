62d11ac0be7cdfc8769948e22c642b9341cc27a5
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2021-12-14 17:34:32 -0600

    Update TicTacToe.java

--- Tic Tac Toe/TicTacToe.java
+++ Tic Tac Toe/TicTacToe.java
@@ -69,4 +69,50 @@ public class TicTacToe
        }
        return false;
    }
+   
+   public boolean checkDiag()
+   {
+       String diagonal = "";
+       String player1Win = "XXX";
+       String player2Win = "OOO";
+       /* 0 1 2             0 1 2
+        0 X          or   0     X
+        1   X             1   X
+        2     X           2 X
+        
+        index[0][0], index[1][1], index[2][2] 
+        or
+        index[0][2], index[1][1], index[2][0]
+        
+       */
+       //for first case scenario
+       for (int r = 0; r < board.length-1; r++) {
+           for (int c = 0; c < board[r].length-1; c++) {
+               //System.out.println(board[c+1][c+1]);
+               diagonal += board[c+1][c+1];
+           }
+           //System.out.println(diagonal);
+           if (diagonal.equals (player1Win) || diagonal.equals (player2Win)) {
+               return true;
+            }
+            diagonal = "";
+       }
+       //for second case scenario
+       diagonal = "";
+       diagonal += board[0+1][2+1];
+       diagonal += board[1+1][1+1];
+       diagonal += board[2+1][0+1];
+       if (diagonal.equals (player1Win) || diagonal.equals (player2Win)) {
+               return true;
+        }
+       return false;
+   }
+   
+   public boolean checkWin()
+   {
+       if (checkRow() || checkCol() || checkDiag()) {
+           return true;
+       }
+       return false;
+   }
 }
