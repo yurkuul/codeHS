34e96a37d06494b1ef90635460c4d1532e260228
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2021-12-15 22:08:38 -0600

    Update TicTacToeTester.java

--- Tic Tac Toe/TicTacToeTester.java
+++ Tic Tac Toe/TicTacToeTester.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class TicTacToeTester
+{
+    public static void main(String[] args)
+    {
+        TicTacToe t = new TicTacToe();
+        Scanner s = new Scanner(System.in);
+        System.out.println ("Current board:");
+        t.printBoard();
+        System.out.println ();
+        
+        System.out.println ("Player 1, eneter the row you would like to go");
+        int row = s.nextInt();
+        while (row > 2 || row < 0) {
+            System.out.println ("That space is invalid!");
+            System.out.println ("Player 1, eneter the row you would like to go");
+            row = s.nextInt();
+        }
+        System.out.println ("Now enter the column you would like to go");
+        int column = s.nextInt();
+        while (column > 2 || column < 0) {
+            System.out.println ("That space is invalid!");
+            System.out.println ("Player 1, eneter the column you would like to go");
+            column = s.nextInt();
+        }
+    }
+}
