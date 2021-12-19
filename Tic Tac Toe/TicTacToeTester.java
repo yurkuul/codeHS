23901b2c0122d508ee16fa19f1c68f9579ec599a
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2021-12-18 18:17:52 -0600

    Update TicTacToeTester.java

--- Tic Tac Toe/TicTacToeTester.java
+++ Tic Tac Toe/TicTacToeTester.java
@@ -33,5 +33,37 @@ public class TicTacToeTester
         System.out.println ("Current board:");
         t.printBoard();
         System.out.println ();
+        while (!t.checkWin() && t.getTurn() < 9) {
+            int theTurn = t.getTurn() % 2 + 1;
+            System.out.println ("Player " + theTurn + ", eneter the row you would like to go");
+            row = s.nextInt();
+            while (row > 2 || row < 0) {
+                System.out.println ("That space is invalid!");
+                System.out.println ("Player " + theTurn + ", eneter the row you would like to go");
+                row = s.nextInt();
+            }
+            System.out.println ("Now enter the column you would like to go");
+            column = s.nextInt();
+            while (column > 2 || column < 0) {
+                System.out.println ("That space is invalid!");
+                System.out.println ("Player " + theTurn + ", eneter the row you would like to go");
+                column = s.nextInt();
+            }
+            if (t.pickLocation(row, column)) {
+                t.takeTurn (row, column);
+            } else {
+                System.out.println ("That spot is already taken!");
+            }
+            System.out.println ();
+            System.out.println ("Current board:");
+            t.printBoard();
+            System.out.println ();
+            if (t.checkWin()) {
+                System.out.println ("Player " + theTurn + " has won!");
+            }
+        }
+        if (!t.checkWin()) {
+            System.out.println ("It is a tie!");
+        }
     }
 }
