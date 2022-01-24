927ddaec4edfe21524319ac7cd1dcbf89e0218bc
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-01-24 07:43:00 -0600

    Create Solid.java

---
+++ funWithSolids/Solid.java
@@ -0,0 +1,24 @@
+public class Solid
+{
+    private String myName;
+
+    public Solid(String name)
+    {
+        myName = name;
+    }
+
+    public String getName()
+    {
+        return myName;
+    }
+
+    public double volume()
+    {
+        return 0;
+    }
+    
+    public double surfaceArea()
+    {
+        return 0;
+    }
+}
