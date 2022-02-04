7450f109435f52b50c26ed05e8913064d31ae0ae
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-02-03 18:30:18 -0600

    Update Pyramid.java

--- funWithSolids/Pyramid.java
+++ funWithSolids/Pyramid.java
@@ -1,11 +1,27 @@
 import java.lang.Math;
 
+/**
+ * Represents a pyramid that has a name. All pyramids have a length, width, and height.
+ * 
+ * @author LZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class Pyramid extends RectangularPrism
 {
     private int length;
     private int width;
     private int height;
     
+    /**
+     * Initializes a new instance of a pyramid with a specified name, length, width, and height.
+     * 
+     * @param name The specified name of the pyramid.
+     * @param length The specified length of the pyramid.
+     * @param width The specified width of the pyramid.
+     * @param height The specified height of the pyramid.
+     * @since 0.0.1
+     */
     public Pyramid(String name, int length, int width, int height) {
         super(name, length, width, height);
         this.length = length;
@@ -13,12 +29,18 @@ public class Pyramid extends RectangularPrism
         this.height = height;
     }
     
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double volume()
     {
         return length*width*height/3.0;
     }
     
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double surfaceArea()
     {
