35a77f6424a56e394fa248b3d5f3a90c989de355
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-02-04 19:36:10 -0600

    Update Sphere.java

--- funWithSolids/Sphere.java
+++ funWithSolids/Sphere.java
@@ -1,20 +1,40 @@
 import java.lang.Math;
 
+/**
+ * Represents a sphere that has a name. All spheres have a radius.
+ * 
+ * @author LZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class Sphere extends Solid
 {
     private int radius;
     
+    /**
+     * Initializes a new instance of a sphere with a specified name and radius.
+     * 
+     * @param name The specified name of the sphere.
+     * @param radius The specified radius of the sphere.
+     * @since 0.0.1
+     */
     public Sphere(String name, int radius) {
         super(name);
         this.radius = radius;
     }
     
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double volume()
     {
         return (4*Math.PI*Math.pow(radius, 3))/3;
     }
     
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double surfaceArea()
     {
