3cccf817b2d23e322c2af65c72aaf0039ac35bc6
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-02-04 19:34:21 -0600

    Update RectangularPrism.java

--- funWithSolids/RectangularPrism.java
+++ funWithSolids/RectangularPrism.java
@@ -1,9 +1,25 @@
+/**
+ * Represents a rectangular prism that has a name. All rectangular prisms have a length, width, and height.
+ * 
+ * @author LZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class RectangularPrism extends Solid
 {
     private int length;
     private int width;
     private int height;
     
+    /**
+     * Initializes a new instance of a rectangular prism with a specified name, length, width, and height.
+     * 
+     * @param name The specified name of the rectangular prism.
+     * @param length The specified length of the rectangular prism.
+     * @param width The specified width of the rectangular prism.
+     * @param height The specified height of the rectangular prism.
+     * @since 0.0.1
+     */
     public RectangularPrism(String name, int length, int width, int height) {
         super(name);
         this.length = length;
@@ -11,12 +27,18 @@ public class RectangularPrism extends Solid
         this.height = height;
     }
     
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double volume()
     {
         return width*height*length;
     }
     
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double surfaceArea()
     {
