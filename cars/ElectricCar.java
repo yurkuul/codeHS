592cf058ddb0081020bee15d7860f667a2ea3a05
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-02-14 17:41:57 -0600

    Update ElectricCar.java

--- cars/ElectricCar.java
+++ cars/ElectricCar.java
@@ -1,18 +1,43 @@
+/**
+ * Represents an electric car.
+ * 
+ * @author LZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class ElectricCar extends Car {
-  
+    /**
+     * Initializes a new instance of an electric car with a specified model and battery level.
+     * 
+     * @param model The specified model of the car.
+     * @param batteryLevel The battery level of the car.
+     * @since 0.0.1
+     */
     public ElectricCar(String model, int batteryLevel){
        super(model, batteryLevel);
     }
-
+  
+    /**
+     * @since 0.0.1
+     */
     @Override
     public double getFuelLevel(){
         return super.getFuelLevel();
     }
-
+    
+    /**
+     * Returns the miles left in the electric car.
+     * 
+     * @return the miles left in the electric car.
+     * @since 0.0.1
+     */
     public double milesLeft(int totalMiles){
         return getFuelLevel()*totalMiles;
     }
-
+    
+    /**
+     * @since 0.0.1
+     */
     @Override
     public String toString(){
         return getModel() + " electric car";
